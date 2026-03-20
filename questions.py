cb69209ac541e15ea3885b0b853f6795673565b4
Agustin Massaccesi <massaccesi.agustin0@gmail.com>
2026-03-20 13:20:22 -0300

    tercer modificacion 'categorias'

--- questions.py
+++ questions.py
@@ -1,18 +1,23 @@
 import random
 
-words = [
-"python",
-"programa",
-"variable",
-"funcion",
-"bucle",
-"cadena",
-"entero",
-"lista",
-]
+words = {
+    "facil": ["bucle","lista","entero","cadena"],
+    "dificil": ["python", "programa", "variable", "funcion"]
+}
+
+print('Categorias disponibles:')
+for categoria in words:
+    print(categoria)
+    
+print()
+categoria_elegida = input('Elegi una categoria:').lower()
+
+while categoria_elegida not in words:
+    print("Categoría inválida")
+    categoria_elegida = input('Elegi una categoria:').lower()
 
 
-word = random.choice(words)
+word = random.choice(words[categoria_elegida])
 guessed = []
 attempts = 6
 
@@ -36,17 +41,20 @@ while attempts > 0:
     if "_" not in progress:
         puntaje += 6
         print("¡Ganaste!")
-        print(f'Puntaje final:{puntaje}')
+        print(f'Puntaje final: {puntaje}')
         break
 
 
     print(f"Intentos restantes: {attempts}")
     print(f"Letras usadas: {', '.join(guessed)}")
 
-    letter = input("Ingresá una letra: ")
+    letter = input("Ingresá una letra: ").lower()
 
     if len(letter) != 1 or not letter.isalpha():
+        print()
         print('Entrada no valida')
+        print()
+        continue
     elif letter in guessed:
         print("Ya usaste esa letra.")
     elif letter in word:
@@ -63,5 +71,5 @@ while attempts > 0:
 else:
         puntaje = 0
         print(f"¡Perdiste! La palabra era: {word}")
-        print(f'Puntaje final:{puntaje}')
+        print(f'Puntaje final: {puntaje}')
         
